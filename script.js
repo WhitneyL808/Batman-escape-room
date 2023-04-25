aad2a4cff76789830cabd1f49b6cf2eabceeb155
Hunter Zepp <122405685+HunterZepp24@users.noreply.github.com>
2023-04-24 21:23:00 -0700

    Added Pages, Styles, and Content

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var countdown = 3600; // the initial time in seconds
+var initialCountdown = 3600; // the initial time in seconds
+var countdown = parseInt(localStorage.getItem("countdown")) || initialCountdown; // get the remaining time from localStorage or use the initial timevar countdown = 3600; // the initial time in seconds
 
 function updateTimer() {
   var minutes = Math.floor(countdown / 60);
@@ -11,19 +12,44 @@ function updateTimer() {
 
 //Clue Time Subtraction
 function subtract5() {
-  countdown -= 300; // subtract 5 minutes from the countdown
-  updateTimer();
+  if (countdown <= 300) {
+    countdown = 0; // set the countdown to zero
+    updateTimer();
+    localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
+  } else {
+    countdown -= 300; // subtract 5 minutes from the countdown
+    updateTimer();
+    localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
+  }
 }
 
 function subtract15() {
-  countdown -= 900; // subtract 15 minutes from the countdown
-  updateTimer();
+  if (countdown <= 900) {
+    countdown = 0; // set the countdown to zero
+    updateTimer();
+    localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
+  } else {
+    countdown -= 900; // subtract 5 minutes from the countdown
+    updateTimer();
+    localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
+  }
+}
+
+// check if the countdown value is valid, and reset it if not
+if (isNaN(countdown) || countdown < 0) {
+  countdown = initialCountdown;
+  localStorage.setItem("countdown", countdown);
 }
 
 // update the timer every second
-setInterval(function () {
+var intervalId = setInterval(function() {
   if (countdown > 0) {
     countdown--;
     updateTimer();
+    localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
+  } else {
+    clearInterval(intervalId); // stop the timer
+    localStorage.removeItem("countdown"); // remove the countdown value from localStorage
+    window.location.href = "failed.html"; // redirect the user to another page
   }
 }, 1000);
