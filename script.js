93606a7bf53b209df6ae04f1e696e7de190916a0
Hunter Zepp <122405685+HunterZepp24@users.noreply.github.com>
2023-04-30 15:34:06 -0700

    Added More Functionality

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function updateTimer() {
 }
 
 //Clue Time Subtraction
-function subtract5() {
-  if (countdown <= 300) {
-    countdown = 0; // set the countdown to zero
+function subtract7() {
+  if (countdown <= 420) {
+    countdown = 1; // set the countdown to zero
     updateTimer();
     localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
   } else {
-    countdown -= 300; // subtract 5 minutes from the countdown
+    countdown -= 420; // subtract 5 minutes from the countdown
     updateTimer();
     localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
   }
@@ -25,7 +25,7 @@ function subtract5() {
 
 function subtract15() {
   if (countdown <= 900) {
-    countdown = 0; // set the countdown to zero
+    countdown = 1; // set the countdown to zero
     updateTimer();
     localStorage.setItem("countdown", countdown); // store the remaining time in localStorage
   } else {
@@ -35,17 +35,66 @@ function subtract15() {
   }
 }
 
-function checkCondition() {
+function checkSolution() {
   var userInput = document.getElementById("user-input").value.toLowerCase();
   var names = ["gains", "ed gains", "edward gains"];
-  var messages = ["Nice try, Batman!", "You're the world's greatest detective? Really?", "Ooh, so close! Try Again!", "Have I bested the Dark Knight? Try Harder!", "You really thought that was me? You're not even close!", "I guess your small brain can't handle this one!"];
+  var messages = ["Nice try, Batman!", "You're the world's greatest detective? Really?", "So close! Try Again!", "Have I bested the Dark Knight? Try Harder!", "You really thought that was me? You're not even close!", "I guess your small brain can't handle this one!"];
   var randomIndex = Math.floor(Math.random() * messages.length);
   var randomMessage = messages[randomIndex];
   if (names.includes(userInput)) {
-    window.location.href = "end.html"; // redirect the user to another page
+    window.location.href = "end.html"; // redirect the user to end page
   } else {
     alert(randomMessage);
     subtract15();
+  }
+}
+
+function checkClue() {
+  var userInput = document.getElementById("user-input").value;
+  if (userInput === "1") {
+    window.location.href = "clue1.html"; // redirect the user to clue1 page
+  } else if (userInput === "2") {
+    window.location.href = "clue2.html"; // redirect the user to clue2 page
+  } else if (userInput === "3") {
+    window.location.href = "clue3.html"; // redirect the user to clue3 page
+  } else if (userInput === "4") {
+    window.location.href = "clue4.html"; // redirect the user to clue4 page
+  } else if (userInput === "5") {
+    window.location.href = "clue5.html"; // redirect the user to clue5 page
+  } else if (userInput === "6") {
+    window.location.href = "clue6.html"; // redirect the user to clue6 page
+  } else if (userInput === "7") {
+    window.location.href = "clue7.html"; // redirect the user to clue7 page
+  } else {
+    alert("I'm not sure where you got that number, Batman!");
+  }
+}
+
+function disableButton(buttonNum) {
+  var button = document.getElementById("button" + buttonNum);
+  button.disabled = true;
+  localStorage.setItem("button" + buttonNum + "Disabled", JSON.stringify(true));
+
+  var link = document.getElementById("link" + buttonNum);
+  var text = link.textContent;
+  var paragraph = document.createElement("p");
+  paragraph.textContent = text;
+  link.parentNode.replaceChild(paragraph, link);
+}
+
+window.onload = function() {
+  for (var i = 1; i <= 3; i++) {
+    var button = document.getElementById("button" + i);
+    var buttonDisabled = JSON.parse(localStorage.getItem("button" + i + "Disabled"));
+    if (buttonDisabled === true) {
+      button.disabled = true;
+
+      var link = document.getElementById("link" + i);
+      var text = link.textContent;
+      var paragraph = document.createElement("p");
+      paragraph.textContent = text;
+      link.parentNode.replaceChild(paragraph, link);
+    }
   }
 }
 
@@ -56,7 +105,7 @@ if (isNaN(countdown) || countdown < 0) {
 }
 
 // update the timer every second
-var intervalId = setInterval(function() {
+var intervalId = setInterval(function () {
   if (countdown > 0) {
     countdown--;
     updateTimer();
@@ -64,6 +113,9 @@ var intervalId = setInterval(function() {
   } else {
     clearInterval(intervalId); // stop the timer
     localStorage.removeItem("countdown"); // remove the countdown value from localStorage
+    localStorage.removeItem("button" + 1 + "Disabled")
+    localStorage.removeItem("button" + 2 + "Disabled")
+    localStorage.removeItem("button" + 3 + "Disabled")
     window.location.href = "failed.html"; // redirect the user to another page
   }
 }, 1000);
